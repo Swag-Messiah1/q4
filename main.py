8fbdbaef97cec3ff88973f132bd5a7100e4f4653
Swag-Messiah1 <146986213+Swag-Messiah1@users.noreply.github.com>
2024-04-12 11:41:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,92 @@
 
+# -*- coding: utf-8 -*-
+"""
+Quarter 4 Project Code
+
+"""
+
+'''
+use append to add to a list - we need to sort list tho
+'''
+
+serie_a_teams = {
+    Juventus,
+    Inter,
+    AC_Milan,
+    Roma,
+    Napoli,
+    Bolonga,
+    Torino,
+    Fiorentina,
+    Lecce,
+    Frosinone,
+    Cagliari,
+    Salernitana,
+    Sassoulo,
+    Udinese,
+    Lazio,
+    Monza,
+    Hellas_Verona,
+    Atalanta,
+    Genoa,
+    Empoli,
+    }
+
+def Juventus():
+
+    
+def Inter():
+
+    
+def AC_Milan():
+
+    
+def Roma():
+
+    
+def Napoli():
+
+    
+def Atalanta():
+
+    
+def Lazio():
+
+    
+def Bolonga():
+
+    
+def Fiorentina():
+
+    
+def Genoa():
+
+    
+def Monza():
+
+    
+def Torino():
+
+    
+def Udinese():
+
+    
+def Lecce():  
+    
+    
+def Hellas_Verona():
+
+    
+def Empoli():
+    
+    
+def Frosinone():
+    
+    
+def Cagliari():
+    
+    
+def Sassoulo():
+    
+    
+def Salernitana():
