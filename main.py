d19b81353f7e662b538f910e1533e1ea57dafe1d
Swag-Messiah1 <146986213+Swag-Messiah1@users.noreply.github.com>
2024-05-07 11:34:45 -0400

    Update main.py
    I switched from a manual in-platform csv reader to a cross platform reader using an excel sheet.

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-#(STILL WORKING ON LEARNING THE FORMAT (CSV)
 """
 Quarter 4 Project Code
 
-"""
 """
 
 '''
 use append to add to a list - we need to sort list tho
 '''
 
-'''
-import pandas as pd
-#import matplotlib.pyplot as plt <-- possile way to import excel sheet into code.
-#    df = pd.read_csv("C:\\Users\\_____\\Downloads\\_____.csv")
-# also could use : C:\Users\samue\OneDrive\Documents
-
-d = {'Team': [1, 2, 3, 4, 7], 'PTS': [14, 10, 9, 6, 5], 'GD': [7, 8, 12, 1, 11]}
-
-df = pd.DataFrame(data=d)
-
-print(df)
-
-'''
 import pandas as pd
 
+
+path = r"C:\Users\samue\Downloads\Q4 FInal Spreadsheet Data - For Project.xlsx"
+#r"C:\Users\samue\OneDrive\Documents\Q4 Project Data Sheet.xlsx" <-- Path for excel sheet on OneDrive :
+#   Claims permission denied.
+
+data = pd.read_excel(path)
+
+print(data)
+
+
+
+'''
 chart = {'|-----Team------|': ['Juventus', 'Inter', 'AC Milan', 'AS Roma', 'Lazio', 'Napoli', 'Genoa', 'Empoli','Lecce','Bolonga','Frosinone','Fiorentina', 'Atlanta','Hellas Verona','Torino', 'Monza','Sassoulo','Udinese','Salernitana','Cagliari', ], '|PTS|': [14, 10, 9, 6, 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 7, 8, 9, 20], '|GD|': [7, 8, 12, 1, 11, 1, 3, 4, 5, 6, 6, 7, 23, 5, 2, 4, 5, 23, 4, 2]}
 
 standings_chart = pd.DataFrame(data=chart)
 
-print(standings_chart)
+print (standings_chart)
 
 row_count = standings_chart.shape[0]
 
-print('\n' + "Number of teams:")
+print('\n' + "Number of Teams:")
 print(row_count)
 
-'''
+column_count = standings_chart.shape[1]
+
+print('\n' + "Number of Categories:")
+print(column_count)
     
     Juventus,
     Inter,
@@ -58,5 +58,4 @@ print(row_count)
     Genoa,
     Empoli,   
 '''    
-
 
