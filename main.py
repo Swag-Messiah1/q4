6d8f22d8f40bff88973c1b86d34219f3ed716143
Swag-Messiah1 <146986213+Swag-Messiah1@users.noreply.github.com>
2024-05-13 12:00:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ Quarter 4 Project Code
 '''
 use append to add to a list - we need to sort list tho
 '''
-
 import pandas as pd
 
 
@@ -16,26 +15,24 @@ path = r"C:\Users\samue\Downloads\Q4 FInal Spreadsheet Data - For Project.xlsx"
 
 data = pd.read_excel(path)
 
+# updating the column value/data 
+data['MP'] = data['MP'].replace({12:13}) 
+  
+# writing into the file 
+data.to_csv(r"C:\Users\samue\Downloads\Q4 FInal Spreadsheet Data - For Project.xlsx", index=False) 
+
 print(data)
 
+matches_played = data.MP
+goal_difference = data.GD
+total_points = data.PTS
 
+#while True:
+    
+  #  chart_change = input(": ")
 
 '''
-chart = {'|-----Team------|': ['Juventus', 'Inter', 'AC Milan', 'AS Roma', 'Lazio', 'Napoli', 'Genoa', 'Empoli','Lecce','Bolonga','Frosinone','Fiorentina', 'Atlanta','Hellas Verona','Torino', 'Monza','Sassoulo','Udinese','Salernitana','Cagliari', ], '|PTS|': [14, 10, 9, 6, 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 5, 7, 8, 9, 20], '|GD|': [7, 8, 12, 1, 11, 1, 3, 4, 5, 6, 6, 7, 23, 5, 2, 4, 5, 23, 4, 2]}
-
-standings_chart = pd.DataFrame(data=chart)
-
-print (standings_chart)
-
-row_count = standings_chart.shape[0]
-
-print('\n' + "Number of Teams:")
-print(row_count)
-
-column_count = standings_chart.shape[1]
-
-print('\n' + "Number of Categories:")
-print(column_count)
+Clubs (Teams) List:
     
     Juventus,
     Inter,
